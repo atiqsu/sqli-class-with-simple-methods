980fa6d10ba594e57fd5c9f0507e41eb377f1daf
atiqur <atiq.cse.cu0506.su@gmail.com>
2016-04-24 13:04:01 +0600

    re-structuring.......

--- cls.sqli.php
+++ cls.sqli.php
@@ -27,6 +27,7 @@
 
 namespace commonSqlOnDev;
 use Mysqli;
+use commonFunction ;
 
 /**
  * Extended mysqli class to serve frequently needed functionality.
@@ -35,6 +36,8 @@ use Mysqli;
  */
 class SQLi extends mysqli
 {
+
+    use commonFunction\commonFunction;
 
     public $dbLink ;
     public $lastError = null ;
@@ -82,16 +85,6 @@ class SQLi extends mysqli
         return $this->dbLink;
     }
 
-    /**
-     * Basic and simple sanitize.
-     * @author Md. Atiqur Rahman <atiq.cse.cu0506.su@gmail.com>
-     * @since 1.0.5
-     * @param $data
-     * @return string
-     */
-    public function sanitizeSimple($data){
-        return htmlentities($data, ENT_QUOTES, 'UTF-8');
-    }
 
     /**
      * Change database within this server.
@@ -1119,87 +1112,7 @@ class SQLi extends mysqli
         return $query ;
     }
 
-    /**
-     * @param $var
-     * @param bool $die
-     * @param null $die_msg
-     * @param bool $varDump
-     */
-    public static function dump($var, $die=true, $die_msg=NULL, $varDump=false){
 
-        if('comment'===$die){   echo '<!-- <pre>';}
-        else{ echo '<pre>';}
-
-        if($varDump===true)  var_dump($var);
-        elseif(is_array($var) && count($var)>0) print_r($var);
-        elseif(is_object($var)) print_r($var);
-        else var_dump($var);
-
-        if($die==='comment') echo '-->';
-        else echo '</pre>';
-        if($die===true)	die('Died from dump helper...'.$die_msg);
-
-    }
-
-    /**
-     * Dump as many as you want .
-     * @author Md. Atiqur Rahman <atiq.cse.cu0506.su@gmail.com>
-     * @since 1.0.0
-     * @param bool $die
-     * @return void
-     */
-    public static function dumps($die=true){
-        echo '<pre>';
-        $args = func_get_args();
-        foreach ($args as $arg){
-            if(is_array($arg) && count($arg)>0) print_r($arg);
-            elseif(is_object($arg)) print_r($arg);
-            else var_dump($arg);
-
-        }
-        echo "</pre>";
-        if($die===true)	die('Died from dumps helper...');
-    }
-
-
-    /**
-     * output string in pre tag.
-     * @author Md. Atiqur Rahman <atiqur@shaficonsultancy.com, atiq.cse.cu0506.su@gmail.com>
-     * @since 1.0.1
-     * @param $var
-     * @param bool $die
-     * @param null $die_msg
-     * @return void
-     */
-    public static function dumpInPre($var, $die=false, $die_msg=NULL){
-
-        echo '<pre>'.$var.'</pre>';
-        if($die===true)	die('Died from sql helper dumper'.$die_msg);
-    }
-
-
-    /**
-     * @author Md. Atiqur Rahman <atiq.cse.cu0506.su@gmail.com>
-     * @since 1.0.0
-     * @param bool $eol
-     * @return void
-     */
-    public static function printBreak($eol = false){
-
-        if($eol) echo PHP_EOL ;
-        else     echo '<br/>';
-    }
-
-    /**
-     * Alias of printBreak method.
-     * @author Md. Atiqur Rahman <atiqur@shaficonsultancy.com, atiq.cse.cu0506.su@gmail.com>
-     * @since 1.0.0
-     * @param bool $eol
-     * @return void
-     */
-    public static function println($eol = false){
-        self::printBreak($eol);
-    }
 
     //=========== very customized functions only project basis : (zend model helper)================
 
